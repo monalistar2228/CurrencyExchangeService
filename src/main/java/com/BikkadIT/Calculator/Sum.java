c54d336d4826a9d133591757b211ea6d84abeff8
monalistar2228 <Monagadhekar007@gmail.com>
2023-04-18 22:14:32 -0700

    First commit by Monali

---
+++ src/main/java/com/BikkadIT/Calculator/Sum.java
@@ -0,0 +1,12 @@
+package com.BikkadIT.Calculator;
+
+public class Sum {
+	
+	public int sum(int a,int b ) {
+		
+		int c = a+b;
+		System.out.println("Addition of a and b  :"+c);
+		return c;
+	}
+
+}
